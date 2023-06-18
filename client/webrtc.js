dce200eea4cac0c9a2e9941a5a4d062a988b8012
Your Name <you@example.com>
2023-06-18 17:44:36 +1000

    client formatting

--- client/webrtc.js
+++ client/webrtc.js
@@ -7,8 +7,8 @@ let uuid;
 
 const peerConnectionConfig = {
   'iceServers': [
-    {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
+    { 'urls': 'stun:stun.stunprotocol.org:3478' },
+    { 'urls': 'stun:stun.l.google.com:19302' },
   ]
 };
 
@@ -26,7 +26,7 @@ function pageReady() {
     audio: true,
   };
 
-  if(navigator.mediaDevices.getUserMedia) {
+  if (navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia(constraints).then(getUserMediaSuccess).catch(errorHandler);
   } else {
     alert('Your browser does not support getUserMedia API');
@@ -42,44 +42,44 @@ function start(isCaller) {
   peerConnection = new RTCPeerConnection(peerConnectionConfig);
   peerConnection.onicecandidate = gotIceCandidate;
   peerConnection.ontrack = gotRemoteStream;
-  
+
   if (localStream) {
     localStream.getTracks().forEach(track => {
       peerConnection.addTrack(track);
     });
   }
 
-  if(isCaller) {
+  if (isCaller) {
     peerConnection.createOffer().then(createdDescription).catch(errorHandler);
   }
 }
 
 
 function gotMessageFromServer(message) {
-  if(!peerConnection) start(false);
+  if (!peerConnection) start(false);
 
   const signal = JSON.parse(message.data);
 
   // Ignore messages from ourself
-  if(signal.uuid == uuid) return;
+  if (signal.uuid == uuid) return;
 
-  if(signal.sdp) {
+  if (signal.sdp) {
     console.log('sdp received')
     peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(() => {
       // Only create answers in response to offers
-      if(signal.sdp.type == 'offer') {
+      if (signal.sdp.type == 'offer') {
         console.log('sdp type is offer')
         peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
       }
     }).catch(errorHandler);
-  } else if(signal.ice) {
+  } else if (signal.ice) {
     peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
   }
 }
 
 function gotIceCandidate(event) {
-  if(event.candidate != null) {
-    serverConnection.send(JSON.stringify({'ice': event.candidate, 'uuid': uuid}));
+  if (event.candidate != null) {
+    serverConnection.send(JSON.stringify({ 'ice': event.candidate, 'uuid': uuid }));
   }
 }
 
@@ -87,7 +87,7 @@ function createdDescription(description) {
   console.log('got description');
 
   peerConnection.setLocalDescription(description).then(() => {
-    serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
+    serverConnection.send(JSON.stringify({ 'sdp': peerConnection.localDescription, 'uuid': uuid }));
   }).catch(errorHandler);
 }
 
